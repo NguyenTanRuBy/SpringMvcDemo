3b353f54de6355f3db1851b3995fd187f124d6de
lcdhuy <56525939+gerhuy66@users.noreply.github.com>
2020-06-18 17:03:10 +0700

    update code

--- DemoSpringMVC/src/com/training/sys/controller/HomeController.java
+++ DemoSpringMVC/src/com/training/sys/controller/HomeController.java
@@ -3,11 +3,13 @@ package com.training.sys.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.training.model.User;
 import com.training.sys.service.UserService;
 
+@Controller
 public class HomeController {
 
 	@Autowired
--- DemoSpringMVC/src/com/training/sys/service/UserServiceImpl.java
+++ DemoSpringMVC/src/com/training/sys/service/UserServiceImpl.java
@@ -2,9 +2,12 @@ package com.training.sys.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.training.model.User;
 import com.training.sys.dao.UserDAO;
 
+@Service
 public class UserServiceImpl implements UserService {
 
 	private UserDAO userDAO;
