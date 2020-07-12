fd88e9bd0ea1cdc50900b6557c6d106ea44c1ebe
NguyenTanRuBy <39164657+NguyenTanRuBy@users.noreply.github.com>
2020-07-12 22:46:14 +0700

    adjust layout

--- DemoSpringMVC/WebContent/WEB-INF/views/js/index.js
+++ DemoSpringMVC/WebContent/WEB-INF/views/js/index.js
@@ -14,12 +14,36 @@ function getAllItem() {
 function categoryClick(button) {
 	var self = this;
 	var requestUrl = '/DemoSpringMVC/item/getItemByCategory?name=' + button.id;
+	
 	axios({
 		method : 'get',
 		url : requestUrl
-	}).then(function(res) {
+	})
+	.then( async function(res) {
 		self.itemByCategory = res.data;
 		console.log(self.itemByCategory);
-	});
+		
+		/// rename category
+		$("#categoryViewer").html("<H3>" + button.id + "</H3>");
+		$("#categoryViewer").append("<div class='card-deck' id='categoryViewerCardDeck'></div>")
+		
+		$("#categoryViewer")[0].scrollIntoView(false);
+		
+		for(let item = 0; item <= self.itemByCategory.length; item++) {
+			$("#categoryViewerCardDeck").append("\
+					<div class='card mb-4'>\
+						<img class='card-img-top' src='" + self.itemByCategory[item].img + "'\
+							alt='Card image'>\
+						<div class='card-body'>\
+							<h4 class='card-title'>"+self.itemByCategory[item].name+"</h4>\
+							<p class='card-text'>Description</p>\
+							<a href='#' class='btn btn-success'>Detail</a>\
+						</div>\
+					</div>")
+}
+		});
+
+		
+		
 }
 
---
+++ DemoSpringMVC/src/com/training/model/ItemTopSale.java
@@ -0,0 +1,18 @@
+package com.training.model;
+
+public class ItemTopSale extends Item {
+
+	private int Amount;
+	
+	public ItemTopSale() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getAmount() {
+		return Amount;
+	}
+
+	public void setAmount(int amount) {
+		Amount = amount;
+	}
+}
